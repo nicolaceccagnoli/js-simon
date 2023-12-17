b3b289d4a435890c4c5ec3e828b7c39c9c7c123a
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2023-12-17 09:59:54 +0100

    Fixed For Loop

--- js/main.js
+++ js/main.js
@@ -12,14 +12,16 @@
 const randomNumbersArray = [];
 console.log('randomNumbersArray', randomNumbersArray, typeof randomNumbersArray);
 
+// Creo una Variabile che prenda dall'HTML il contenitore dei Numeri del PC
+const pcNumbers = document.querySelector('#pc-numbers');
+
 // Creo una Variabile che intrappoli la Funzione per la generazione dei numeri casuali
-let randomPcNumbers = numberGenarator(100, 1, randomNumbersArray);
+let randomPcNumbers = numberGenarator(100, 1, randomNumbersArray, pcNumbers);
 
 // Creo una Funzione che gestisca l'inserimento dei numeri nell'array che li conterrà
-function numberGenarator  (max, min, array) {
+function numberGenarator (max, min, array, div) {
     // Creo un ciclo che generi 5 numeri casuali 
     for (i = 1; i <= 5; i++) {
-
         // Creo una Variabile che rappresenti i numeri casuali del PC e ci inserisco la Funzione
         let numbers = randomNumbers(max, min);
         console.log('Numeri del PC', numbers, typeof numbers);
@@ -28,17 +30,29 @@ function numberGenarator  (max, min, array) {
         let foundInArray = array.includes(numbers);
         console.log('foundInArray', foundInArray, typeof foundInArray); 
 
-        //  Creo un ciclo che controlli che i numeri inseriti nell'Array siano sempre diversi 
+        //  Creo un ciclo che controlli che i numeri inseriti nell'Array siano sempre si 
         while (foundInArray == true) {
             numbers = randomNumbers(max, min);
             console.log(numbers);
 
             foundInArray = array.includes(numbers);
         }
-
-
+        
         // Pusho i numeri dentro l'array
         array.push(numbers);
+
+        // Creo un elemento all'interno del Contenitore dei numeri
+        let numberContainer = document.createElement('div');
+
+        // Stampo all'interno del contenitore i numeri estratti
+        numberContainer.innerHTML += numbers;
+        
+        // "Appendo" i Piccoli Contenitori al Grande Contenitore
+        div.append(numberContainer);
+
+        // Aggiundo dello stile al Grande Contenitore
+        div.classList.add('d-flex', 'justify-content-around', 'p-5', 'text-danger', 'bg-dark', 'fs-2','border', 'rounded');
+
     }
 }
 
