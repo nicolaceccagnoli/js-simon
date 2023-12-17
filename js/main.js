ba7a9c2f60ba9a6e08100f7959f9e5fa868b3d59
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2023-12-17 09:02:46 +0100

    Impostato Esercizio

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*
+
+    1) Visualizzare in pagina 5 numeri casuali;
+        -- Parte un Timer di 30 secondi
+    2) Dopo 30 secondi i numeri scompaiono
+        -- l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt()
+    3) Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+    
+*/
