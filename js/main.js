dcc1d6066cab9d0e9f77b91b0e0aa3abc5bc188c
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2023-12-17 09:15:52 +0100

    Add Function and For Loop for Pc's Number

--- js/main.js
+++ js/main.js
@@ -5,5 +5,19 @@
     2) Dopo 30 secondi i numeri scompaiono
         -- l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt()
     3) Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-    
+
 */
+
+// Creo un ciclo che generi 5 numeri casuali 
+for (i = 1; i <= 5; i++) {
+    // Creo una Variabile che rappresenti i 5 numeri casuali
+    let pcNumbers = randomNumbers(100, 1);
+    console.log('Numeri del PC', pcNumbers, typeof pcNumbers);
+}
+
+// Creo una Funzione che generi numeri casuali
+function randomNumbers (max, min) {
+    const randNum = Math.floor(Math.random() * (max - min + 1)) + min;
+    
+    return randNum;
+}
